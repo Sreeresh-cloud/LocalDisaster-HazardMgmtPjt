813d8d2cea84492852479109c51b90a288e0a211
Sreeresh-cloud <sreereshss2@gmail.com>
2024-09-22 20:08:54 +0530

    First Push

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome to the Local Hazard & Disaster Management System</h1>
+      <h1>Welcome to the Dashboard</h1>
       <p>This is the home page. You can access the system features from here.</p>
     </div>
   );
